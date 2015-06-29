91b910663567b4f1f5f4410804056fd823799a11
Carlos Rodriguez <carlosrod.furlan@gmail.com>
2015-06-28 22:03:37 -0600

    Modificacion de la pagina web a mostrar

--- httpCarlos.c
+++ httpCarlos.c
@@ -19,11 +19,11 @@ using namespace std;
 
 char response[] = "HTTP/1.1 200 OK\r\n"
 "Content-Type: text/html; charset=UTF-8\r\n\r\n"
-"<!DOCTYPE html><html><head><title>Bye-bye baby bye-bye</title>"
+"<!DOCTYPE html><html><head><title>Sopes2 - Tarea2</title>"
 "<style>body { background-color: #111 }"
 "h1 { font-size:4cm; text-align: center; color: black;"
 " text-shadow: 0 0 2mm red}</style></head>"
-"<body><h1>Goodbye, world!</h1></body></html>\r\n";
+"<body><h1>Sistemas Operativos 2</h1><h2>201212947 - Carlos Arturo Rodriguez Furlan</h2></body></html>\r\n";
  
 int main()
 {
